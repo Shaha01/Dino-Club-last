10379c3a363d42504d3a4537fe6f614caa0ac5eb
Shahriyor <abrorovshahriyor9@gmail.com>
2024-12-26 20:09:48 +0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    let btn = document.getElementById("scrollBtn");
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
